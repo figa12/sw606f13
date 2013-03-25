70589efb1b619d2b927c06339896b16ba164d773
Jacob Karstensen Wortmann <jacob@wortmann.dk>
2013-03-25 10:53:44 +0100

    Fixet lidt :)

--- source/Train/src/dk/aau/cs/giraf/train/opengl/GameActivity.java
+++ source/Train/src/dk/aau/cs/giraf/train/opengl/GameActivity.java
@@ -2,7 +2,6 @@ package dk.aau.cs.giraf.train.opengl;
 
 import dk.aau.cs.giraf.train.R;
 
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.ClipData;
@@ -19,136 +18,175 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 public class GameActivity extends Activity {
-    
-    private GlView openGLView;    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-    	this.setContentView(R.layout.activity_game);
-    	
-    	this.setListeners();
-    	
-        this.openGLView = (GlView)findViewById(R.id.openglview);
-        
-        this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE); // TODO investigate API level problems and write results in comment
-    }
-    
-    /* Sets touch and drag listeners. This is temporary and only for proof of concept */
-    private void setListeners() {
-    	
-        findViewById(R.id.Cart1LeftImageView).setOnTouchListener(new TouchListener());
-        findViewById(R.id.Cart1RightImageView).setOnTouchListener(new TouchListener());
-        findViewById(R.id.Cart2LeftImageView).setOnTouchListener(new TouchListener());
-        findViewById(R.id.Cart2RightImageView).setOnTouchListener(new TouchListener());
-        findViewById(R.id.Spot1LeftImageView).setOnTouchListener(new TouchListener());
-        findViewById(R.id.Spot2RightImageView).setOnTouchListener(new TouchListener());
-        findViewById(R.id.Spot3LeftImageView).setOnTouchListener(new TouchListener());
-        findViewById(R.id.Spot4RightImageView).setOnTouchListener(new TouchListener());
-        //findViewById(R.id.myimage9).setOnTouchListener(new TouchListener());
-        //findViewById(R.id.StationLayout).setOnDragListener(new DragListener());
-        findViewById(R.id.Cart1LeftLayout).setOnDragListener(new DragListener());
-        findViewById(R.id.Cart1RightLayout).setOnDragListener(new DragListener());
-        findViewById(R.id.Cart2LeftLayout).setOnDragListener(new DragListener());
-        findViewById(R.id.Cart2RightLayout).setOnDragListener(new DragListener());
-        findViewById(R.id.Spot1LeftLayout).setOnDragListener(new DragListener());
-        findViewById(R.id.Spot2RightLayout).setOnDragListener(new DragListener());
-        findViewById(R.id.Spot3LeftLayout).setOnDragListener(new DragListener());
-        findViewById(R.id.Spot4RightLayout).setOnDragListener(new DragListener());
-        //findViewById(R.id.TrainLayout).setOnDragListener(new DragListener());
-    }
-    
-    @Override
-    protected void onPause() {
-        super.onPause();
-        this.openGLView.onPause();
-    }
-    
-    @Override
-    protected void onResume() {
-        super.onResume();
-        this.openGLView.onResume();
-    }
-    
-    /** 
-     * A touch listener that starts a drag event.
-     * There should also be a receiver implementing {@link OnDragListener}.
-     * 
-     *  @see DragListener
-     */
-    private final class TouchListener implements OnTouchListener {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                ClipData data = ClipData.newPlainText("", "");
-                DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
-                view.startDrag(data, shadowBuilder, view, 0);
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-    
-    /** A drag listner implementing an onDrag() method that runs when something is dragged to it. */
-    private final class DragListener implements OnDragListener {
-        private Drawable enterShape;
-        private Drawable normalShape;
-        
-        public DragListener() {
-            Resources resources = getResources();
-            
-            this.enterShape = resources.getDrawable(R.drawable.shape_droptarget);
-            this.normalShape = resources.getDrawable(R.drawable.shape);
-        }
-        
-        @Override
-        public boolean onDrag(View v, DragEvent event) {
-            if (event.getLocalState() != null) {
-                View draggedView = (View) event.getLocalState();
-                ViewGroup ownerContainer = (ViewGroup) draggedView.getParent();
-                switch (event.getAction()) {
-                case DragEvent.ACTION_DRAG_STARTED:
-                    // makes the draggedview invisible in ownerContainer
-                    draggedView.setVisibility(View.INVISIBLE);
-                    break;
 
-                case DragEvent.ACTION_DRAG_ENTERED:
-                    // Change the background of droplayout(purely style)
-                    v.setBackgroundDrawable(enterShape); // FIXME code is
-                                                         // deprecated, use new
-                    break;
+	private GlView openGLView;
 
-                case DragEvent.ACTION_DRAG_EXITED:
-                    // Change the background back when exiting droplayout(purely
-                    // style)
-                    v.setBackgroundDrawable(normalShape); // FIXME code is
-                                                          // deprecated, use new
-                    break;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.activity_game);
 
-                case DragEvent.ACTION_DROP:
-                    // Dropped, assigns the draggedview to the dropcontainer if
-                    // the container does not already contain a view.
-                    FrameLayout dropContainer = (FrameLayout) v;
-                    Object tag = dropContainer.getTag();
+		this.setListeners();
 
-                    if (tag == null) {
-                        ownerContainer.removeView(draggedView);
-                        ownerContainer.setTag(null);
-                        dropContainer.addView(draggedView);
-                        dropContainer.setTag("filled");
-                    }
-                    break;
+		this.openGLView = (GlView) findViewById(R.id.openglview);
 
-                case DragEvent.ACTION_DRAG_ENDED:
-                    // Makes the draggedview visible again after the view has
-                    // been moved or the drop wasn't valid.
-                    v.setBackgroundDrawable(normalShape); // FIXME code is
-                                                          // deprecated, use new
-                    draggedView.setVisibility(View.VISIBLE);
-                    break;
-                }
-            }
-            return true;
-        }
-    }
+		this.getWindow().getDecorView()
+				.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE); // TODO
+																			// investigate
+																			// API
+																			// level
+																			// problems
+																			// and
+																			// write
+																			// results
+																			// in
+																			// comment
+	}
+
+	/*
+	 * Sets touch and drag listeners. This is temporary and only for proof of
+	 * concept
+	 */
+	private void setListeners() {
+		// TouchListeners
+		findViewById(R.id.Cart1LeftImageView).setOnTouchListener(
+				new TouchListener());
+		findViewById(R.id.Cart1RightImageView).setOnTouchListener(
+				new TouchListener());
+		findViewById(R.id.Cart2LeftImageView).setOnTouchListener(
+				new TouchListener());
+		findViewById(R.id.Cart2RightImageView).setOnTouchListener(
+				new TouchListener());
+		findViewById(R.id.Spot1LeftImageView).setOnTouchListener(
+				new TouchListener());
+		findViewById(R.id.Spot2RightImageView).setOnTouchListener(
+				new TouchListener());
+		findViewById(R.id.Spot3LeftImageView).setOnTouchListener(
+				new TouchListener());
+		findViewById(R.id.Spot4RightImageView).setOnTouchListener(
+				new TouchListener());
+
+		// Draglisteners
+		findViewById(R.id.Cart1LeftLayout)
+				.setOnDragListener(new DragListener());
+		findViewById(R.id.Cart1RightLayout).setOnDragListener(
+				new DragListener());
+		findViewById(R.id.Cart2LeftLayout)
+				.setOnDragListener(new DragListener());
+		findViewById(R.id.Cart2RightLayout).setOnDragListener(
+				new DragListener());
+		findViewById(R.id.Spot1LeftLayout)
+				.setOnDragListener(new DragListener());
+		findViewById(R.id.Spot2RightLayout).setOnDragListener(
+				new DragListener());
+		findViewById(R.id.Spot3LeftLayout)
+				.setOnDragListener(new DragListener());
+		findViewById(R.id.Spot4RightLayout).setOnDragListener(
+				new DragListener());
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		this.openGLView.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		this.openGLView.onResume();
+	}
+
+	/**
+	 * A touch listener that starts a drag event. There should also be a
+	 * receiver implementing {@link OnDragListener}.
+	 * 
+	 * @see DragListener
+	 */
+	private final class TouchListener implements OnTouchListener {
+		@Override
+		public boolean onTouch(View view, MotionEvent motionEvent) {
+			if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+				ClipData data = ClipData.newPlainText("", "");
+				DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
+						view);
+				view.startDrag(data, shadowBuilder, view, 0);
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	/**
+	 * A drag listner implementing an onDrag() method that runs when something
+	 * is dragged to it.
+	 */
+	private final class DragListener implements OnDragListener {
+		private Drawable enterShape;
+		private Drawable normalShape;
+
+		public DragListener() {
+			Resources resources = getResources();
+
+			this.enterShape = resources
+					.getDrawable(R.drawable.shape_droptarget);
+			this.normalShape = resources.getDrawable(R.drawable.shape);
+		}
+
+		@Override
+		public boolean onDrag(View v, DragEvent event) {
+			if (event.getLocalState() == null) {
+				// do nothing, maybe return false..
+				return true;
+			}
+			
+			View draggedView = (View) event.getLocalState();
+			ViewGroup ownerContainer = (ViewGroup) draggedView.getParent();
+			
+			switch (event.getAction()) {
+			case DragEvent.ACTION_DRAG_STARTED:
+				// makes the draggedview invisible in ownerContainer
+				draggedView.setVisibility(View.INVISIBLE);
+				break;
+
+			case DragEvent.ACTION_DRAG_ENTERED:
+				// Change the background of droplayout(purely style)
+				v.setBackgroundDrawable(enterShape); // FIXME code is
+														// deprecated, use new
+				break;
+
+			case DragEvent.ACTION_DRAG_EXITED:
+				// Change the background back when exiting droplayout(purely
+				// style)
+				v.setBackgroundDrawable(normalShape); // FIXME code is
+														// deprecated, use new
+				break;
+
+			case DragEvent.ACTION_DROP:
+				// Dropped, assigns the draggedview to the dropcontainer if
+				// the container does not already contain a view.
+				FrameLayout dropContainer = (FrameLayout) v;
+				Object tag = dropContainer.getTag();
+
+				if (tag == null) {
+					ownerContainer.removeView(draggedView);
+					ownerContainer.setTag(null);
+					dropContainer.addView(draggedView);
+					dropContainer.setTag("filled");
+				}
+				break;
+
+			case DragEvent.ACTION_DRAG_ENDED:
+				// Makes the draggedview visible again after the view has
+				// been moved or the drop wasn't valid.
+				v.setBackgroundDrawable(normalShape); // FIXME code is
+														// deprecated, use new
+				draggedView.setVisibility(View.VISIBLE);
+				break;
+
+			}
+			return true;
+		}
+	}
 }
