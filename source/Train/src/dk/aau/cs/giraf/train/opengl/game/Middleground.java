9a24c63078f070ebaf02df3479520e132e939cd1
unknown <Simon Reedtz Olesen>
2013-04-09 16:04:00 +0200

    Bakkerne er awesome og er inde i spillet nu

--- source/Train/src/dk/aau/cs/giraf/train/opengl/game/Middleground.java
+++ source/Train/src/dk/aau/cs/giraf/train/opengl/game/Middleground.java
@@ -3,11 +3,14 @@ package dk.aau.cs.giraf.train.opengl.game;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.content.Context;
+import android.renderscript.Program.TextureType;
+import dk.aau.cs.giraf.train.R;
 import dk.aau.cs.giraf.train.opengl.Color;
 import dk.aau.cs.giraf.train.opengl.Coordinate;
 import dk.aau.cs.giraf.train.opengl.GameDrawer;
 import dk.aau.cs.giraf.train.opengl.RenderableMatrix;
 import dk.aau.cs.giraf.train.opengl.Square;
+import dk.aau.cs.giraf.train.opengl.Texture;
 
 public final class Middleground extends RenderableGroup {
     
@@ -16,24 +19,28 @@ public final class Middleground extends RenderableGroup {
     }
 
     private RenderableMatrix sequence = new RenderableMatrix();
-    private Square square = new Square(100f, 100f);
+  //  private Square square = new Square(100f, 100f);
+    private Texture hills = new Texture(8096f, 752f);
+    
     
     @Override
     public void load() {
-        this.sequence.addCoordinate(500f, 300f, GameData.MIDDLEGROUND-500f);
-        this.sequence.addCoordinate(198.92f, -87f, GameData.MIDDLEGROUND);
-        
-        for (float i = 0f; i <= 8000f; i += 100f) {
-            this.sequence.addRenderableMatrixItem(square, new Coordinate(i, 0f, 0f), Color.Blue);
-            i += 100f;
-            this.sequence.addRenderableMatrixItem(square, new Coordinate(i, 0f, 0f), Color.Green);
+    //  this.sequence.addCoordinate(500f, 300f, GameData.MIDDLEGROUND-500f);
+      //  this.sequence.addCoordinate(198.92f, -87f, GameData.MIDDLEGROUND);
+
+    	  this.sequence.addCoordinate(-1275f, 47f, GameData.MIDDLEGROUND);
+    	  
+        for (float i = 0f; i <= 8000f; i += this.hills.getWidth()-30f) {
+            this.sequence.addRenderableMatrixItem(this.hills, new Coordinate(i, 0f, 0f));
         }
+        
+        this.hills.loadTexture(super.gl, super.context, R.drawable.texture_hills);
     }
 
     @Override
     public void draw() {
+        //super.translateAndDraw(this.sequence);
         super.translateAndDraw(this.sequence);
-        
         this.sequence.move(GameData.pixelMovementForThisFrame, 0f);
     }
 }
